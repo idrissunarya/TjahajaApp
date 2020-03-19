71702b6c73fbf446dca7e5f6236eb7879391edb9
idrissunarya <antimainstream53@gmail.com>
2020-03-19 17:17:34 +0700

    update

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+
 const mongoose = require('mongoose');
+// Connect MongoDB with mongoose
+mongoose.connect('mongodb://localhost:27017/TjahajaApp');
+
+const Todo = require('./models/Todo');
 
 // initialize our express app
 const app = express();
-
-// Connect MongoDB with mongoose
-mongoose.connect('mongodb://localhost:27017/TjahajaApp');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -22,9 +24,26 @@ app.get('/dashboard', function(req, res) {
     res.render('dashboard/dashboard');
 });
 
+// Route Add Menu
+app.get('/dashboard/menu', function(req, res) {
+    res.render('dashboard/pages/menus/index.pug');
+});
+
+// Route todos
+app.get('/dashboard/todos', function(req, res) {
+    console.log('getting all todos');
+    Todo.find({}).exec(function(err, todos){
+        if(err) {
+            res.send('error has occured');
+        } else {
+            console.log(todos);
+            res.json(todos);
+        }
+    });
+});
+
 // Start Server
 var port = 3000;
 app.listen(port, () => {
     console.log('server is running port number ' + port);
 });
-
